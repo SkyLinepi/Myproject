f3ef56386fb5b1e6fdf949e6d9888f827837a7a8
Ballyuyio <Ballyuyio@gmail.com>
2025-11-18 21:42:05 +0700

    update
    pongtai

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int sky = 0;
+        for ()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
