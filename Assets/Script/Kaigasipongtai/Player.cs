a04d147734f9ef8a26170279f5e2eed53cab0f6d
Ballyuyio <Ballyuyio@gmail.com>
2025-12-03 02:24:41 +0700

    Anima
    อนิเม

--- Assets/Script/Kaigasipongtai/Player.cs
+++ Assets/Script/Kaigasipongtai/Player.cs
@@ -2,25 +2,72 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private Animator _animator;
     public Rigidbody2D rb2D;
     public float force = 1f;
-     
+    private Vector2 input;
+    private Vector2 lastMoveDirection;
+    private bool facingLeft = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        _animator = GetComponent<Animator>();
         rb2D = GetComponent<Rigidbody2D>();
     }
 
     public void Move()
     {
-        float moveInput = Input.GetAxis("Horizontal");
-        Vector2 direction = new Vector2(moveInput, 0).normalized;
+        float moveXInput = Input.GetAxis("Horizontal");
+        float moveYInput = Input.GetAxis("Vertical");
+        Vector2 direction = new Vector2(moveXInput, moveYInput).normalized;
         rb2D.AddForce(direction * force);
+    }
+
+    public void Flip()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+        facingLeft = !facingLeft;
+    }
+
+    void ProccessInput()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        if ((moveX == 0 && moveY == 0) && (input.x != 0 || input.y != 0))
+        {
+            lastMoveDirection = input;
+        }
+
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+
+        input = input.normalized;
+    }
+
+    void Animate()
+    {
+        _animator.SetFloat("MoveX", input.x);
+        _animator.SetFloat("MoveY", input.y);
+        _animator.SetFloat("MoveMagnitude", input.sqrMagnitude);
+        _animator.SetFloat("LastMoveX", lastMoveDirection.x);
+        _animator.SetFloat("LastMoveY", lastMoveDirection.y);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         Move();
+        ProccessInput();
+        Animate();
+        if (input.x < 0 && !facingLeft || input.x > 0 && facingLeft)
+        {
+            Flip();
+        }
+        
     }
 }
