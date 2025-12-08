fb5b307be0ad28cc1126f2fdcc58344072d6b6b1
SkyLinepi <136251080+SkyLinepi@users.noreply.github.com>
2025-12-08 10:09:19 +0700

    Harpoonหมดแล้วนะไม่ต้องแก้แล้ว

--- Assets/Script/Despawner.cs
+++ Assets/Script/Despawner.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class Despawner : MonoBehaviour
 {
+    public GameManager GM;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        GM = FindObjectOfType<GameManager>();
         Destroy(gameObject, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D cd)
+    {
+        if (cd.CompareTag("fish"))
+        {
+            FishMovement fm = cd.GetComponent<FishMovement>();
+            fish fishdata = fm.FishIdentity;
+            GM.TriggerMiniGame(fishdata);
+        }
     }
 }
